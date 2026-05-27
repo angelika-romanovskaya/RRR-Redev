53157ac1624049dcff35e985aeec65aadaf8dcbe
Angelika <angelika_romanovskaya_2002@mail.ru>
2026-05-27 22:39:43 +0300

    react state task 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { ParentComponent } from './components/ParentComponent'
-import { SiblingComponent } from './components/SiblingComponent'
+import { UserProfile } from './components/UserProfile'
+import { ToDoList } from './components/ToDoList'
+import { Basket } from './components/Basket'
 
 function App() {
-  return <ParentComponent/>
+  return <>
+    <UserProfile/>
+    <ToDoList/>
+    <Basket/>
+  </>
 }
 
 export default App
--- src/components/ChildComponent.jsx
+++
@@ -1,3 +0,0 @@
-export const ChildComponent = ({name, count}) => {
-    return <h1>Привет, {name}. Значение счетчика: {count}</h1>
-}
